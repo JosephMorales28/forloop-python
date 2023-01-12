9f6c4322b282d6b9fcbd0bc2e37ed2190abfb4fc
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2023-01-12 09:53:55 +0800

    Add files via upload

---
+++ for loop.py
@@ -0,0 +1,7 @@
+str="the quick brown fox jump down over the lazy dog"
+count=0
+for x in str:
+    if x== "g":
+       count+=1
+print(count)
+#if the value of x is g the output is one why because in the str value has one letter g and it output to one but if you want to change the value of x is equal to o the output will be 5 because in the str value have o letters and it counted on how many letter o in the str value
